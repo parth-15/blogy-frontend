6cfe8817271eb4b2688fc14b73d2fd622614dbc1
Parth Patel <parth4321patel@gmail.com>
2021-09-08 11:10:42 +0530

    Add index.html file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>Code is written by parth patel</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
